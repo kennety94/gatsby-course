9666cdda9832d182d64c86b01bcd2b943dc9382e
Kennety <kennetyandersson@gmail.com>
2019-12-15 11:36:57 -0300

    Criação básica do componente profile

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+    <div className="Profile-wrapper">
+        <h1>Kennety Dantas</h1>
+        <h2>Desenvolvedor Front-end</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    </div>
+)
+
+export default Profile
