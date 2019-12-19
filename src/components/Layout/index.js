5dc023a859d2fe9606e0ca63e43bdea44198984d
Kennety <kennetyandersson@gmail.com>
2019-12-19 20:35:24 -0300

    Componte avatar estilizado

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,29 +2,21 @@ import React from "react"
 import PropTypes from "prop-types"
 import Profile from "../Profile"
 
-import styled from 'styled-components'
+import * as S from './styled'
+
 import GlobalStyles from '../../styles/global'
 
-const LayoutWraper = styled.section`
-  display: flex
-`
-const LayoutMain = styled.main`
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75rem 0 20 rem;
-  width: 100%
-`
 
 const Layout = ({ children }) => {
 
   return (
-    <LayoutWraper>
+    <S.LayoutWraper>
       <GlobalStyles />
       <aside>
-        <Profile></Profile>
+        <Profile />
       </aside>
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWraper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWraper>
   )
 }
 
