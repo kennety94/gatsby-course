cb12d82747049a8fe8ba353d998b84fbb9252e99
Kennety <kennetyandersson@gmail.com>
2019-12-22 18:54:20 -0300

    Criação da MenuBar

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,11 @@
-import React from "react"
-import PropTypes from "prop-types"
-import Sidebar from "../Sidebar"
+import React from 'react'
+import PropTypes from 'prop-types'
+import Sidebar from '../Sidebar'
+import MenuBar from '../MenuBar'
 
-import * as S from "./styled"
+import * as S from './styled'
 
-import GlobalStyles from "../../styles/global"
+import GlobalStyles from '../../styles/global'
 
 const Layout = ({ children }) => {
   return (
@@ -12,6 +13,7 @@ const Layout = ({ children }) => {
       <GlobalStyles />
       <Sidebar />
       <S.LayoutMain>{children}</S.LayoutMain>
+      <MenuBar />
     </S.LayoutWraper>
   )
 }
